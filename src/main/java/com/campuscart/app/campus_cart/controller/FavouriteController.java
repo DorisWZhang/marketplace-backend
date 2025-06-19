bdc28b18fc5baced5cba3b5393c10f862516a968
Doris Zhang <doriszhang912@gmail.com>
2025-06-18 20:01:27 -0400

    fix favouriting and unfavouriting

--- src/main/java/com/campuscart/app/campus_cart/controller/FavouriteController.java
+++ src/main/java/com/campuscart/app/campus_cart/controller/FavouriteController.java
@@ -7,15 +7,9 @@ import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.campuscart.app.campus_cart.model.Favourite;
 import com.campuscart.app.campus_cart.repository.FavouriteRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
    
 
 
--- src/main/java/com/campuscart/app/campus_cart/repository/FavouriteRepository.java
+++ src/main/java/com/campuscart/app/campus_cart/repository/FavouriteRepository.java
@@ -1,6 +1,10 @@
 package com.campuscart.app.campus_cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+
+
 import com.campuscart.app.campus_cart.model.Favourite;
 import java.util.List;
 import java.util.Optional;
@@ -8,5 +12,8 @@ import java.util.Optional;
 public interface FavouriteRepository extends JpaRepository<Favourite, Long> {
     List<Favourite> findByUserId(Long userId);
     Optional<Favourite> findByUserIdAndItemId(Long userId, Long itemId);
+
+    @Modifying
+    @Transactional
     void deleteByUserIdAndItemId(Long userId, Long itemId);
 }
